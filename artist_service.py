89b59972cdee63ff21fcf9a0b61a16f59119a36d
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-08 10:42:38 -0700

    Some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,18 @@
+artist_info = {
+          'R3hab': {
+              'image': 'R3hab.png',
+              'spotify_link': 'https://open.spotify.com/embed/artist/6cEuCEZu7PAE9ZSzLLc2oQ'
+          },
+          'Avicii': {
+              'image': 'Avicii.jpg',
+              'spotify_link': 'https://open.spotify.com/embed/artist/1vCWHaC5f2uS3yhpwWbIA6'
+          },
+          'Kshmr': {
+            'image':'Kshmr.png',
+            'spotify_link': 'https://open.spotify.com/embed/artist/2wX6xSig4Rig5kZU6ePlWe'
+        },
+        'Tiesto': {
+          'image':'Tiesto.png',
+          'spotify_link': "https://open.spotify.com/embed/artist/2o5jDhtHVPhrJdv3cEQ99Z"
+         }
+}
